689c091b29d0a3ef47b8bec339da8c467be1acba
himitsu1111 <lettercatcher@bk.ru>
2016-10-31 15:55:15 +0300

    .

--- src/main/java/hello/MyController.java
+++ src/main/java/hello/MyController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,21 +11,23 @@ import java.util.ArrayList;
 
 @Controller
 public class MyController {
-
+    private ProField pf;
+    ArrayList<ProField> al;
     @RequestMapping(value="/1", method= RequestMethod.GET)
     public String greetingForm(Model model) {
+        al = new ArrayList<>();
         model.addAttribute("1", new Greeting());
-        model.addAttribute("3", new ArrayList<String>());
+        model.addAttribute("3", new ProField());
         return "1";
     }
 
     @RequestMapping(value="/1", method=RequestMethod.POST)
     public String greetingSubmit(@ModelAttribute ProField proField, Greeting greeting, Model model) {
-        ArrayList<ProField> al;
-        al = new ArrayList<>();
-        proField.setStr1("asdasd");
-        proField.setStr2("cvxcvxcv");
-        al.add(proField);
+
+
+//        proField.setStr1("asdasd");
+//        proField.setStr2("cvxcvxcv");
+        al.add(pf);
         al.add(proField);
         al.add(proField);
 
@@ -34,11 +37,13 @@ public class MyController {
         return "2";
     }
     @RequestMapping(value="/3", method=RequestMethod.POST)
-    public void greetingSubmit2(@ModelAttribute ProField proField, Model model) {
-
-
+    public void greetingSubmit2(@ModelAttribute ProField proField, Model model, HttpServletRequest request) {
+        String s = request.getParameter("s1");
+        String s2 = request.getParameter("s2");
+        proField.setStr1(s);
+        proField.setStr2(s2);
+        pf = proField;
         model.addAttribute("3", proField);
-
     }
 
 }
